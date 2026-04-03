e2dd34d07fe929ca2036baf99065649226a0ee75
shiinedev <shiinecadde17@gmail.com>
2026-04-03 09:40:01 +0100

    feat(ui): create ui package structure

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,32 @@
-import { nextJsConfig } from "@repo/eslint-config/next-js";
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import turboPlugin from "eslint-plugin-turbo";
+import tseslint from "typescript-eslint";
+import onlyWarn from "eslint-plugin-only-warn";
 
-/** @type {import("eslint").Linter.Config[]} */
-export default nextJsConfig;
+/**
+ * A shared ESLint configuration for the repository.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ * */
+export const config = [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      turbo: turboPlugin,
+    },
+    rules: {
+      "turbo/no-undeclared-env-vars": "warn",
+    },
+  },
+  {
+    plugins: {
+      onlyWarn,
+    },
+  },
+  {
+    ignores: ["dist/**"],
+  },
+];
