bd0bd3e43ba15af51154b60a0b42eb05fe502b2a
shiinedev <shiinecadde17@gmail.com>
2026-04-03 17:34:14 +0100

    added types in api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,15 +1,34 @@
 import express from "express";
-
+import type { User } from "@repo/types/auth"
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+const users: User[] = [
+    {
+        id: "1",
+        username: "john_doe",
+        email: "john@gmail.com"
+    },
+    {
+        id: "2",
+        username: "jane_smith",
+        email: "jane@gmail.com"
+    }
+]
 
 app.get("/health", (req, res) => {
-    res.json({ ok: true, message: "Api is running!" ,
+    res.json({
+        ok: true, message: "Api is running!",
         timestamp: new Date().toISOString(),
+    });
+});
+
+app.get("/users", (req, res) => {
+    res.json({
+        message: "users fetched successfully",
+        data: users,
     });
 });
 
---
+++ packages/types/src/index.ts
@@ -0,0 +1,9 @@
+
+export type { User } from "./auth"
+
+export type PORT = number;
+export type APIResponse = {
+    ok: boolean;
+    message: string;
+    timestamp: string;
+};
